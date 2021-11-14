398727213f85bfa13e979bd608d0443c4a64ce9d
Dmitriy Dovguchev <anothermind1282@gmail.com>
2021-11-14 17:41:26 +0300

    add: 8 and 9

--- 9. sound-board/script.js
+++ 9. sound-board/script.js
@@ -1,4 +1,4 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong', 'warcraft']
 
 sounds.forEach(sound => {
     const btn = document.createElement('button')
